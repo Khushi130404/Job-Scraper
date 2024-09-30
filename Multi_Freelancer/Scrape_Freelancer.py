b1ba75e24f76bb2df86d46cf551fe21670b5d2f3
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-09-30 15:46:41 +0530

    Freelencer-2

--- Multi_Freelancer/Scrape_Freelancer.py
+++ Multi_Freelancer/Scrape_Freelancer.py
@@ -87,7 +87,7 @@ def scrape_page():
             time.sleep(5)
 
 # Loop through pagination links to scrape data from multiple pages
-for i, page_url in enumerate(pagination_links[:4]):
+for i, page_url in enumerate(pagination_links[:3]):
     try:
         driver.get(page_url)
         time.sleep(5)
